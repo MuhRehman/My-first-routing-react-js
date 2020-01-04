dd9a03b8846fa86f7f4c5a7c0fb8136d9d30e02e
paki_jan@yahoo.com <paki_jan@yahoo.com>
2020-01-04 23:18:00 +0500

    Updates

---
+++ precache-manifest.97250a40eee360e9b91c1aac2a2d31de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ef16bb41fd64215755909cac293a7c",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "fad982a5ff3b4c3cf2a0",
+    "url": "/hello/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "04c509c298df49d7458b",
+    "url": "/hello/static/js/2.9e7890c8.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "/hello/static/js/2.9e7890c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fad982a5ff3b4c3cf2a0",
+    "url": "/hello/static/js/main.c1484a3a.chunk.js"
+  },
+  {
+    "revision": "10ca6e045b50f42d13cf",
+    "url": "/hello/static/js/runtime-main.57152622.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
